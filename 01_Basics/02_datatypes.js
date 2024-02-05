4c3842b3992f8cd6b6f959d34500a206b4b1272d
Hulash Kumar <hulashkrcps@gmail.com>
2024-02-06 00:23:10 +0530

    Add 02_datatypes.js

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treats all javascipt code as newer version
+
+// alert("hello") // can't use because we are using nodejs and not browser
+
+// console.log(3+6); console.log(4+8)
+// console.log(7+
+//     5);
+// code readablility should be high
+
+
+let name="hulash" // string
+let age=22 // number
+let isLoggedIn = false // Boolean
+let state;
+let temperature = null
+
+// number => 2^53
+// bigint
+// string => "" or ''
+// boolean => true or false
+// null => standalone value
+// undefined => variable is declared but not initialized
+// symbol => to uniquely identify components
+
+
+// object
+
+console.log(typeof "hulash"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
